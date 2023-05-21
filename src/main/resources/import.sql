094585921e53760f26f7e9b495626f0ddd53b01e
Marta <marta.dulibic@photomath.com>
2023-05-21 21:19:31 +0200

    fixed generating ids go from 1

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,11 +4,11 @@ INSERT INTO sort (id, name, description) VALUES (3, 'Pinot Noir', 'Pinot Noir is
 INSERT INTO sort (id, name, description) VALUES (4, 'Sauvignon Blanc', 'Sauvignon Blanc is a white wine grape variety known for its vibrant acidity and refreshing character. It exhibits flavors of citrus fruits, green apple, gooseberry, and sometimes grassy or herbal notes.');
 INSERT INTO sort (id, name, description) VALUES (5, 'Syrah', 'Syrah (also known as Shiraz) is a red wine grape variety that produces full-bodied wines with intense flavors of blackberries, black pepper, and spices. It often has a rich and velvety texture.');
 
-INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (1, 'Chardonnay', 2021, 13.5, 'Dry', 'High', 'Vineyard XYZ', 10.0, 'Perfect with seafood', 'Chardonnay is a versatile white wine...', 29.99, 1);
-INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (2, 'Merlot', 2020, 14.2, 'Semi Dry', 'Medium', 'Vineyard ABC', 16.0, 'Pairs well with red meat', 'Merlot is a red wine known for its...', 39.99, 2);
-INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (3, 'Pinot Noir', 2019, 12.8, 'Semi Sweet', 'High', 'Vineyard LMN', 14.0, 'Great with grilled salmon', 'Pinot Noir is a delicate red wine...', 34.99, 3);
-INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (4, 'Sauvignon Blanc', 2022, 11.5, 'Dry', 'Medium', 'Vineyard PQR', 8.0, 'Refreshing with salads', 'Sauvignon Blanc is a crisp white wine...', 24.99, 1);
-INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (5, 'Syrah', 2018, 14.8, 'Dry', 'High', 'Vineyard DEF', 18.0, 'Pairs well with spicy dishes', 'Syrah (or Shiraz) is a bold red wine...', 42.99, 2);
+INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (100, 'Chardonnay', 2021, 13.5, 'Dry', 'High', 'Vineyard XYZ', 10.0, 'Perfect with seafood', 'Chardonnay is a versatile white wine...', 29.99, 1);
+INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (102, 'Merlot', 2020, 14.2, 'Semi Dry', 'Medium', 'Vineyard ABC', 16.0, 'Pairs well with red meat', 'Merlot is a red wine known for its...', 39.99, 2);
+INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (103, 'Pinot Noir', 2019, 12.8, 'Semi Sweet', 'High', 'Vineyard LMN', 14.0, 'Great with grilled salmon', 'Pinot Noir is a delicate red wine...', 34.99, 3);
+INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (104, 'Sauvignon Blanc', 2022, 11.5, 'Dry', 'Medium', 'Vineyard PQR', 8.0, 'Refreshing with salads', 'Sauvignon Blanc is a crisp white wine...', 24.99, 1);
+INSERT INTO wine (id, name, harvest, alcohol_percentage, maltster, quality, vineyard, temperature_of_serving, gastro_recommendation, description, price, sort_id)VALUES (105, 'Syrah', 2018, 14.8, 'Dry', 'High', 'Vineyard DEF', 18.0, 'Pairs well with spicy dishes', 'Syrah (or Shiraz) is a bold red wine...', 42.99, 2);
 
 
 INSERT INTO wine_order (id, order_status, total_price, payment_method, creation_timestamp) VALUES (1, 'CREATED', 39.99, 'CARD', CURRENT_TIMESTAMP);
@@ -17,14 +17,14 @@ INSERT INTO wine_order (id, order_status, total_price, payment_method, creation_
 INSERT INTO wine_order (id, order_status, total_price, payment_method, creation_timestamp) VALUES (4, 'DELIVERED', 99.99, 'CARD', CURRENT_TIMESTAMP);
 INSERT INTO wine_order (id, order_status, total_price, payment_method, creation_timestamp) VALUES (5, 'CANCELED', 29.99, 'CASH', CURRENT_TIMESTAMP);
 
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (1, 2);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (1, 3);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (1, 4);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 1);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 2);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 2);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 4);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (3, 3);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (4, 3);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (4, 3);
-INSERT INTO wineorder_wine (order_id, wine_id) VALUES (5, 5);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (1, 102);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (1, 103);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (1, 104);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 100);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 102);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 102);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (2, 104);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (3, 103);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (4, 103);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (4, 103);
+INSERT INTO wineorder_wine (order_id, wine_id) VALUES (5, 105);
